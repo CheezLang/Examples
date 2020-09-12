61919f489667f4d4984e6030db8ead1c95fb0c5c
Nimaoth <nicolas.otremba@web.de>
2020-09-13 00:46:59 +0200

    imgui working

--- vulkan_window/build.ps1
+++ vulkan_window/build.ps1
@@ -1,4 +1,4 @@
 [CmdletBinding()]
 param ([Parameter(ValueFromRemainingArguments)] [string[]] $Passthrough)
 Copy-Item "../libraries/GLFW/glfw/lib/glfw3.dll" "./bin"
-&cheezc ./src/main.che --out ./bin --name vulkan_window --time --error-source --modules ../libraries/vulkan/vulkan ../libraries/GLFW/glfw @Passthrough
+&cheezc ./src/main.che --out ./bin --name vulkan_window --time --error-source --modules ../libraries/vulkan/vulkan ../libraries/GLFW/glfw ../libraries/imgui/src @Passthrough
