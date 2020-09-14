e592a576cc6962b27349b5d86e0a433a3c0ed5e2
Nimaoth <nicolas.otremba@web.de>
2020-09-15 00:58:00 +0200

    fixed triangle winding order, moved stats to imgui window

--- vulkan_window/shaders/triangle.vert.glsl
+++ vulkan_window/shaders/triangle.vert.glsl
@@ -9,7 +9,7 @@ layout(location = 0) out vec3 fragColor;
 layout(location = 1) out vec2 uv;
 
 void main() {
-    gl_Position = vec4(position.xyz * vec3(1, -1, 0.1) + vec3(0, +0.25, 0.5), 1.0);
-    fragColor = normal * vec3(1, 1, 1) * 0.5 + 0.5;
+    gl_Position = vec4(position.xyz * vec3(1, -1, -0.1) + vec3(0, +0.25, 0.5), 1.0);
+    fragColor = normal * vec3(1, 1, -1) * 0.5 + 0.5;
     uv = texCoord;
 }
