89b5b34e37560089fc96367c088ae281315b8a9a
Nimaoth <nicolas.otremba@web.de>
2020-09-16 22:43:28 +0200

    multiple descriptor sets, replaced vertex buffers with storage buffers

--- vulkan_window/shaders/triangle.vert.glsl
+++ vulkan_window/shaders/triangle.vert.glsl
@@ -1,18 +1,10 @@
 #version 450
 #extension GL_ARB_separate_shader_objects : enable
 
-layout(location = 0) in vec3 position;
-layout(location = 1) in vec3 normal;
-layout(location = 2) in vec2 texCoord;
-
-layout(location = 0) out vec3 fragColor;
-layout(location = 1) out vec2 uv;
-
-
 struct Vertex {
-    vec3 position;
-    vec3 normal;
-    vec2 texCoord;
+    vec4 position;
+    vec4 normal;
+    vec4 texCoord;
 };
 
 struct DrawData {
@@ -21,16 +13,23 @@ struct DrawData {
     mat4 proj;
 };
 
-layout(binding = 0, row_major) readonly buffer DrawDataBuffer {
+layout(set = 0, binding = 0, row_major) uniform DrawDataBuffer {
     DrawData drawData;
 };
 
-layout(binding = 1) readonly buffer VertexBuffer {
+layout(set = 1, binding = 0) readonly buffer VertexBuffer {
     Vertex vertices[];
 };
 
+layout(location = 0) out vec3 fragColor;
+layout(location = 1) out vec2 uv;
+
 void main() {
+    vec3 position = vertices[gl_VertexIndex].position.xyz;
+    vec3 normal = vertices[gl_VertexIndex].normal.xyz;
+    vec2 texCoord = vertices[gl_VertexIndex].texCoord.xy;
+
     gl_Position = drawData.proj * drawData.view * drawData.model * vec4(position.xyz * vec3(1, 1, -1) - vec3(0, 0.3, 0), 1);
-    fragColor = normal * vec3(1, 1, 1) * 0.5 + 0.5;
-    uv = texCoord;
+    fragColor = normal.xyz * vec3(1, 1, 1) * 0.5 + 0.5;
+    uv = texCoord.xy;
 }
