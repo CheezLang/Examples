3ac9f20573345bf46323be6f726d844bedfd6557
Nimaoth <nicolas.otremba@web.de>
2020-09-16 15:10:31 +0200

    added mesh, refactored a lot

--- vulkan_window/shaders/triangle.vert.glsl
+++ vulkan_window/shaders/triangle.vert.glsl
@@ -2,14 +2,25 @@
 #extension GL_ARB_separate_shader_objects : enable
 
 layout(location = 0) in vec3 position;
-layout(location = 1) in vec2 texCoord;
-layout(location = 2) in vec3 normal;
+layout(location = 1) in vec3 normal;
+layout(location = 2) in vec2 texCoord;
 
 layout(location = 0) out vec3 fragColor;
 layout(location = 1) out vec2 uv;
 
+struct DrawData {
+    mat4 model;
+    mat4 view;
+    mat4 proj;
+};
+
+layout(binding = 0, row_major) readonly buffer DrawDataBuffer {
+    DrawData drawData;
+};
+
+
 void main() {
-    gl_Position = vec4(position.xyz * vec3(1, -1, -0.1) + vec3(0, +0.25, 0.5), 1.0);
-    fragColor = normal * vec3(1, 1, -1) * 0.5 + 0.5;
+    gl_Position = drawData.proj * drawData.view * drawData.model * vec4(position.xyz * vec3(1, 1, -1) - vec3(0, 0.3, 0), 1);
+    fragColor = normal * vec3(1, 1, 1) * 0.5 + 0.5;
     uv = texCoord;
 }
