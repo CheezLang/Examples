4e56509cf30f87d7a9956863a73359ed368b1084
Nimaoth <nicolas.otremba@web.de>
2020-09-22 21:37:26 +0200

    pbrt-cheez: implemented more stuff

--- run.ps1
+++ run.ps1
@@ -1,5 +1,5 @@
 . ./config.ps1
 Push-Location "./$example/bin"
-&"./$example.exe" -out image.bpm -scene ../scenes/scene.scn
+&"./$example.exe" -out image.bpm -scene ../scenes/scene.scn -log
 # &"./$example.exe"
 Pop-Location
